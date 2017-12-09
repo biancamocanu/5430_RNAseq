6490ac25e2d640670cdf2741c61203cd3748a8e0
biancamocanu <bianca.mocanu@uconn.edu>
2017-12-09 16:52:09 -0500

    fixed script up to stringtie

--- pipe.sh
+++ pipe.sh
@@ -36,15 +36,13 @@ hg19index="/tempdata3/MCB5430/genomes/hg19/hisat_gencode_tran/hg19_gencode"
 hg19chromInfo="/tempdata3/MCB5430/genomes/hg19/hg19_chromInfo.txt"
 gencode="/tempdata3/MCB5430/annotations/hs/bed/hg19_gencode_ENSG_geneID.bed"
 gencode_GTF="/tempdata3/MCB5430/annotations/hs/GTF/gencode.v19.annotation_pc.gtf"
-
-chr12="/tempdata3/MCB5430/genomes/hg19/fasta/chr12.fa"
 hg19="/tempdata3/MCB5430/genomes/hg19/fasta/hg19.fa"
-TSSbackground="/tempdata3/MCB5430/midterm/midterm/hg19_unique_TSSonly_bkgrnd.txt"
 outPATH="/tempdata3/MCB5430/Bianca_Final/processed_data/"  #using this folder since there's a size limit for the individual accounts' storage
 fastqfiles=$(find ${inPATH} -maxdepth 1 -type f)
 
 #===================================================================================================================================================
 # Generating output directories
+
 mkdir /tempdata3/MCB5430/Bianca_Final/
 mkdir $outPATH
 cd $outPATH
@@ -53,9 +51,8 @@ mkdir ./fastqc_reports
 mkdir ./hisat_OUT
 	mkdir ./hisat_OUT/sam
 	mkdir ./hisat_OUT/bam
-	mkdir ./hisat_OUT/bed
 	mkdir ./hisat_OUT/bedgraphs
-	
+mkdir ./logfiles
 mkdir ./stringtie_OUT
 
 #====================================================================================================================================================
@@ -88,58 +85,40 @@ for file in $fastqfiles
 
 			
 			echo "Starting HISAT2 alignment to the hg19 gencode tran assembly"
+			cd $outPATH/hisat_OUT/sam
+			hisat2 -p4 --dta -x $hg19index -U $outPATH/Qtrimmed_fastq/${prefix}_trimmed.fastq -S ./${prefix}.sam  2>&1
 			
-		
 			echo "Alignment to human genome (hg19) complete for $(basename $file)!"
 
 			echo "Generating BAM file..."
-			samtools view -S -b ${prefix}_chr12.sam > ${prefix}_chr12.bam  # sam to bam conversion
-
-			samtools sort -l 9 -n  ${prefix}_chr12.bam -T ${prefix} -o ${prefix}_chr12.sorted.bam  #this sorts the bam file so that it occupies less space
+			samtools view -S -b ${prefix}.sam > ../bam/${prefix}.bam  # sam to bam conversion  2>&1
+			cd ../bam
+			samtools sort -@ 4 ${prefix}.bam ${prefix}_sorted  2>&1
+			rm ${prefix}.bam 
+			
 			echo "BAM file generated!"
 
-			echo "Generating BED file..."
-			bedtools bamtobed -i ${prefix}_chr12.sorted.bam > ${prefix}_chr12.bed
-			echo "BED file generated!"
-			echo "Sorting BED file"
-			sortBed -i ${prefix}_chr12.bed > ${prefix}_chr12_sorted.bed
-			echo "Generating bedgraph file..."
-			bedtools genomecov -ibam ${prefix}_chr12.sorted.bam -bg > ${prefix}.bedgraph #generates the bedgraph from bam directly
+			echo "Generating bedgraph files for browser data visualization..."
+			bedtools genomecov -bga -split -ibam ./${prefix}_sorted.bam -g $hg19chromInfo > ../bedgraphs/${prefix}_IGV.bedgraph 2>&1
+			
+			echo "Preparing bedgraphs for Genome Browser..."
+		
+			if [ $prefix=="E2_rep1" ] || [ $prefix=="E2_rep2" ]
+			then
+				awk -v NAME="$prefix" 'BEGIN { print "browser position chr12:5,289,521-5,291,937"
+				print "track type=bedGraph name=\""NAME"\" description=\""NAME"\" visibility=full windowingFunction=maximum color=0,0,125"}
+				{print $0}' ${prefix}_IGV.bedgraph > ${prefix}_UCSC.bedgraph
+			else
+				awk -v NAME=$prefix 'BEGIN { print "browser position chr12:5,289,521-5,291,937"
+				print "track type=bedGraph name=\""NAME"\" description=\""NAME"\" visibility=full windowingFunction=maximum color=125,0,125"}
+				{print $0}' ${prefix}_IGV.bedgraph > ${prefix}_UCSC.bedgraph
+			fi
+		
+			echo "Genome Browser bedgraphs generated!"
+			
 			echo "Bedgraph file generated!"
 
-#==============================================================================================================================================================
-# Comment this section if you want to keep all these files
-#==============================================================================================================================================================
-			echo "Cleaning up temporary files"
-			rm ${prefix}_clipped.fastq # partially processed file
-			rm ${prefix}_preprocessed.fastq # fully preprocessed fastq file - it occupies a lot of space
-			rm ${prefix}.sam # large file as well, virtually useless once converted to bam
-			rm ${prefix}_chr12.sam # subset of the file above, only for the assigned chromosome
-			rm ${prefix}_chr12.bam # unsorted bam file
-			rm ${prefix}_chr12.bed # unsorted bed file
-		
-		fi
-#==============================================================================================================================================================
-
-		echo "Preparing bedgraphs for Genome Browser..."
-		
-		if [ $prefix=="treatA_chip_rep1" ] || [ $prefix=="treatA_chip_rep2" ]
-		then
-			awk -v NAME="$prefix" 'BEGIN { print "browser position chr12:5,289,521-5,291,937"
-			print "track type=bedGraph name=\""NAME"\" description=\""NAME"\" visibility=full windowingFunction=maximum color=0,0,125"}
-			{print $0}' ${prefix}.bedgraph > ${prefix}_header.bedgraph
-		elif [ $prefix=="treatAB_chip_rep1" ] || [ $prefix=="treatAB_chip_rep2" ]
-		then
-			awk -v NAME=$prefix 'BEGIN { print "browser position chr12:5,289,521-5,291,937"
-			print "track type=bedGraph name=\""NAME"\" description=\""NAME"\" visibility=full windowingFunction=maximum color=125,0,125"}
-			{print $0}' ${prefix}.bedgraph > ${prefix}_header.bedgraph
-		elif [ $prefix=="Input" ]
-		then
-			awk -v NAME=$prefix 'BEGIN { print "browser position chr12:5,289,521-5,291,937"
-			print "track type=bedGraph name=\""NAME"\" description=\""NAME"\" visibility=full windowingFunction=maximum color=125,0,0"}
-			{print $0}' ${prefix}.bedgraph > ${prefix}_header.bedgraph
+	
 		fi
 		
-		echo "Genome Browser bedgraphs generated!"
-		
-	done | tee -a ${outPATH}logfiles/log.txt
+	done | tee -a ${outPATH}/logfiles/log.txt
