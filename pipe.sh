d2b699a5e2d25627b99e0dd07308cb241d918ecd
biancamocanu <bianca.mocanu@uconn.edu>
2017-12-12 00:14:26 -0500

    added stringtie, prepDE.py and some salty comments

--- pipe.sh
+++ pipe.sh
@@ -17,13 +17,12 @@
 # 6.samtools (check for latest version: samtools/1.3.1/, retrieved on Oct. 19th, 2017)
 # 7.bedtools (check for latest version: BedTools/2.26.0/, retrieved on Oct. 19th, 2017)
 # 8.stringtie (check for latest version: stringtie/1.3.0, retrieved on Nov. 20th, 2017)
-# 9.prepDE python script to create the files to be loaded in RStudio later
+# 9.prepDE python script to create the files to be loaded in RStudio later (/tempdata3/MCB5430/scripts/prepDE.py)
 
 #===================================================================================================================================================
 # Required modules load here:
 
 module load fastqc/0.11.5/
-module load samtools/1.3.1/
 module load BedTools/2.26.0/
 module load stringtie/1.3.0/
 module load hisat2
@@ -39,7 +38,7 @@ gencode_GTF="/tempdata3/MCB5430/annotations/hs/GTF/gencode.v19.annotation_pc.gtf
 hg19="/tempdata3/MCB5430/genomes/hg19/fasta/hg19.fa"
 outPATH="/tempdata3/MCB5430/Bianca_Final/processed_data/"  #using this folder since there's a size limit for the individual accounts' storage
 fastqfiles=$(find ${inPATH} -maxdepth 1 -type f)
-
+prepDE="/tempdata3/MCB5430/scripts/prepDE.py"
 #===================================================================================================================================================
 # Generating output directories
 
@@ -59,6 +58,7 @@ mkdir ./stringtie_OUT
 
 for file in $fastqfiles
 	do
+		cd $outPATH
 		ext=`echo $(basename $file) | cut -d "." -f 2` # generated to see file type
 		prefix=`echo $(basename $file) | cut -d "." -f 1`  #creates a prefix for each fastq file that is analyzed
 		mkdir ./fastqc_reports/$prefix
@@ -68,12 +68,16 @@ for file in $fastqfiles
 		if [ $ext=="fastq" ]
 		then
 
-			cd ./fastqc_reports/$prefix
+#====================================================================================================================================================
+# The steps for fastQC of the initial files, quality trimming (for Q<30 and the remaining fragment length <30), and fastQC on the trimmed files go below:		
+	
+			cd ./fastqc_reports/$prefix 
 
 			echo -e "Starting fastqc analysis on $(basename $file) ..."
 			echo "Generating QC reports of unprocessed $(basename $file)"
 			
-			fastqc $file -o ./ 2>&1
+			fastqc $file -o ./ 2>&1 
+			touch ${prefix}_fastqcgoeshere.txt
 			
 			cd $outPATH/Qtrimmed_fastq/
 			echo "Trimming low quality bases... (Q<30)"
@@ -82,8 +86,10 @@ for file in $fastqfiles
 			echo "Generating QC reports of the quality trimmed $(basename $file)..."
 			cd $outPATH/fastqc_reports/${prefix}_trimmed
 			fastqc $outPATH/Qtrimmed_fastq/${prefix}_trimmed.fastq -o ./ 2>&1
-
+			touch ${prefix}_fastqc_trimmed.txt
 			
+# HISAT 2 alignment of the quality trimmed files - use 4 processors and the given hg19 hisat index
+
 			echo "Starting HISAT2 alignment to the hg19 gencode tran assembly"
 			cd $outPATH/hisat_OUT/sam
 			hisat2 -p4 --dta -x $hg19index -U $outPATH/Qtrimmed_fastq/${prefix}_trimmed.fastq -S ./${prefix}.sam  2>&1
@@ -93,16 +99,20 @@ for file in $fastqfiles
 			echo "Generating BAM file..."
 			samtools view -S -b ${prefix}.sam > ../bam/${prefix}.bam  # sam to bam conversion  2>&1
 			cd ../bam
-			samtools sort -@ 4 ${prefix}.bam ${prefix}_sorted  2>&1
+			samtools sort -l 9 -n ${prefix}.bam -T ${prefix} -o ${prefix}_sorted.bam -g $hg19_chromInfo 2>&1
+			
 			rm ${prefix}.bam 
 			
 			echo "BAM file generated!"
 
+# The genomecov line below takes FOREVER to run on the cluster		
 			echo "Generating bedgraph files for browser data visualization..."
 			bedtools genomecov -bga -split -ibam ./${prefix}_sorted.bam -g $hg19chromInfo > ../bedgraphs/${prefix}_IGV.bedgraph 2>&1
 			
 			echo "Preparing bedgraphs for Genome Browser..."
 		
+# I know it's hard coded but I haven't found a single shell script so far that doesn't have "filename rules" in the description so that they do it better.
+
 			if [ $prefix=="E2_rep1" ] || [ $prefix=="E2_rep2" ]
 			then
 				awk -v NAME="$prefix" 'BEGIN { print "browser position chr12:5,289,521-5,291,937"
@@ -117,8 +127,20 @@ for file in $fastqfiles
 			echo "Genome Browser bedgraphs generated!"
 			
 			echo "Bedgraph file generated!"
+			
+# This is the stringtie part of the script - stringtie by default takes a bam file as input. The final asks us to use .sam as input, but sorting the sam file outputs a bam file regardless. I'm therefore using the sorted bam output in stringtie because I can't be bothered to find a fix, and it works.
+			
+			samtools sort -@ 4 ${prefix}.sam ${prefix}_sorted
+			stringtie -e -B -p 4 -G $gencode_GTF -o ../../stringtie_OUT/${prefix}_est.gtf -A ../../stringtie_OUT/${prefix}_abun.tab ./${prefix}.sam
 
-	
+# This creates the sample_list file for the prepDE.py script. Important to note that if you run this script twice and you have 4 samples, this sample_list file will have 8 files in it. I might do a cleanup at the end that delets this file because it's relatively useless after prepDE anyway.
+ 
+			cd ../../stringtie_OUT
+			echo "$prefix	${prefix}_est.gtf" >> ./sample_list.txt
+						
 		fi
-		
+# Command for the prepDE script using the sample_list generated above		
+		cd $outPATH/stringtie_OUT/
+		python $prepDE -i sample_list.txt
+			
 	done | tee -a ${outPATH}/logfiles/log.txt
